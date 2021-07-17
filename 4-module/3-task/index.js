c0f4029f42b4f09fb892603a92ad749528d34cd8
Daria Gladysheva <gladdaria@gmail.com>
2021-07-17 12:31:29 +0300

    занятие 4

---
+++ 4-module/3-task/index.js
@@ -0,0 +1,23 @@
+function highlight(table) {
+  for (let i = 1; i < table.rows.length; i++) {
+    if ((table.rows[i].cells[3].dataset.available) === 'true') {
+      table.rows[i].classList.add("available");
+    }else if ((table.rows[i].cells[3].dataset.available) === 'false') {
+      table.rows[i].classList.add('unavailable');
+    }else {
+    table.rows[i].hidden = true;
+    }       
+  }
+  for (let i = 1; i < table.rows.length; i++) {
+    if (table.rows[i].cells[2].innerHTML === 'm'){
+      table.rows[i].classList.add('male');
+      } else {
+          table.rows[i].classList.add('female');
+      }
+    }
+    for (let i = 1; i < table.rows.length; i++) {
+      if (table.rows[i].cells[1].innerHTML < 18){
+        table.rows[i].style.textDecoration = 'line-through';
+      }
+    }
+}
