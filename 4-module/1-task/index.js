19f051c5e794ecb9d640d12176bcb1bc53642752
Daria Gladysheva <gladdaria@gmail.com>
2021-07-18 21:12:52 +0300

    исправления к задаче 1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -2,8 +2,7 @@ function makeFriendsList(friends) {
   let unorderedList = document.createElement('ul');
    
    for (let friend of friends) {
-     let friendName = `<li>${friend.firstName} ${friend.lastName}</li>`
-     unorderedList.innerHTML += friendName;
+     unorderedList.innerHTML += `<li>${friend.firstName} ${friend.lastName}</li>`;
    }
   
    return unorderedList;
