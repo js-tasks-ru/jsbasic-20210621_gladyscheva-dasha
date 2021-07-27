6b2433c8a9949d2cc25e9eb26713d867e6691bdd
Daria Gladysheva <gladdaria@gmail.com>
2021-07-27 22:46:17 +0300

    задача 3 занятия 5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,43 @@
 function initCarousel() {
-  // ваш код...
+  const carouselInner = document.querySelector('.carousel__inner');
+  const carouselSlides = document.querySelectorAll('.carousel__slide');
+  const next = document.querySelector('.carousel__arrow_right');
+  const prev = document.querySelector('.carousel__arrow_left')
+  
+  let slidesNumber = 4;
+  let i = 0;
+  let slideWidth = carouselInner.offsetWidth;
+
+  prev.style.display = 'none';
+
+  next.addEventListener('click', () => {
+    i++
+    carouselInner.style.transform = `translateX(${-slideWidth * i}px)`
+
+    if(i > 0) {
+    prev.style.display = '';
+  } 
+
+    if (i >= slidesNumber - 1) {
+      next.style.display = 'none';
+    } else {
+      next.style.display = '';
+    }
+  })
+
+  prev.addEventListener('click', () => {
+    i--
+    carouselInner.style.transform = `translateX(${-slideWidth * i}px)`
+
+    if(i == 0) {
+      prev.style.display = 'none';
+  } 
+
+    if (i >= slidesNumber - 1) {
+      next.style.display = 'none';
+    } else {
+      next.style.display = '';
+    }
+  })
 }
+
