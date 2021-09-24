7fa352f83b859998eea1e5e31a791438acd6367c
Daria Gladysheva <gladdaria@gmail.com>
2021-09-24 17:57:31 +0300

    6 module solved

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,41 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+    
+    const removeButton = `<button class="remove-button">[x]</button>`;
+    this.elem.addEventListener ('click', (event) => this.onClick(event));
+    
+    const tableHead = this.elem.insertAdjacentHTML ('afterbegin', `
+    <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+          </tr>
+        </thead>
+        `)
+    
+    for (let data of rows) {
+      this.elem.insertAdjacentHTML('afterbegin', `
+        <tr>
+          <td>${data.name}</td>
+          <td>${data.age}</td>
+          <td>${data.salary}</td>
+          <td>${data.city}</td>
+          <td>${removeButton}</td>
+        </tr>
+        `);
+     }
+     
+    }
+    onClick(event){
+      if (event.target.className != "remove-button")
+        return;
+        
+        let row  = event.target.closest('tr');
+        row.remove();
+    }
   }
-}
